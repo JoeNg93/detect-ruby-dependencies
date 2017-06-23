4a807b76dd96227a2f44337df5ba44d562851516
Joe Nguyen <ntuandung93@gmail.com>
2017-06-23 13:49:35 +0300

    Add docstring for script

--- parse_ruby_project.py
+++ parse_ruby_project.py
@@ -1,10 +1,29 @@
+"""
+This file is used to get all the dependencies of a Ruby project and save it to a file
+Support either a path to project or a remote git link
+
+Usage:
+    $python3 parse_ruby_project.py [path_to_project_or_git_link]
+
+Output:
+    Result will be saved to:
+        [project_name]_internal_deps.list (internal dependencies)
+        [project_name]_external_deps.list (external dependencies)
+    Contents of output:
+        [source_path]:[dependency_path]:[type_of_require]
+    Example:
+        /project/a.rb:/project/b.rb:require_internal
+        /project/a.rb:/External/my_external.rb:require_external
+"""
 import os
 import sys
 import re
 
 
-# Various patterns needed
 class RegexPattern:
+    """
+    Various patterns needed
+    """
     git_pattern = re.compile(
         r"^(https:\/\/github\.com\/|git@github\.com:)[a-zA-Z0-9]+\/(.*)\.git$")
     dependency_pattern = re.compile(r"^\s*require.*['|\"](.*)['|\"]")
@@ -37,15 +56,16 @@ def main():
         internal_deps_file.name, external_deps_file.name))
     for file in ruby_files:
         deps = get_deps_from_ruby_file(file)
+        # Only take path from root_folder
         source_path = file[file.index(root_folder) - 1:]
         for dep in deps:
-            if dep in ruby_files:
+            if dep in ruby_files:  # Internal dependency
                 # -1 to take the leading slash "/"
                 dep_path = dep[dep.index(root_folder) - 1:]
                 internal_deps_file.write(
                     ":".join([source_path, dep_path, "require_internal"]))
                 internal_deps_file.write("\n")
-            else:
+            else:  # External depedency
                 dep_path = "/External/" + os.path.basename(dep)
                 external_deps_file.write(
                     ":".join([source_path, dep_path, "require_external"]))
@@ -55,10 +75,27 @@ def main():
 
 
 def is_valid_git_link(link_path):
+    """
+    Check to see whether a path is valid git link
+    Support both SSH and HTTPS protocol
+
+    Args:
+        link_path: Path to remote git link
+    Returns:
+        True or False
+    """
     return bool(RegexPattern.git_pattern.match(link_path))
 
 
 def get_ruby_files_from_root_folder(folder_name):
+    """
+    Get all ruby files (recursively) from a root folder
+
+    Args:
+        folder_name: Root folder name
+    Returns:
+        List of ruby files (absolute path)
+    """
     ruby_files = []
     for current_dir, dirs, files in os.walk(folder_name):
         files_with_path = map(lambda file: os.path.abspath(
@@ -68,14 +105,38 @@ def get_ruby_files_from_root_folder(folder_name):
 
 
 def get_ruby_files_from_list(file_list):
+    """
+    Get only ruby files from the list of files
+
+    Args:
+        file_list: List of files
+    Returns:
+        List of ruby files
+    """
     return [file for file in file_list if is_ruby_file(file)]
 
 
 def is_ruby_file(file_name):
+    """
+    Check to see whether the file is ruby file or not
+
+    Args:
+        file_name: Name of the file
+    Returns:
+        True or False
+    """
     return file_name.endswith(".rb")
 
 
 def get_deps_from_ruby_file(file_name):
+    """
+    Get all the dependencies for a Ruby file
+
+    Args:
+        file_name: Name of file to pull out dependencies
+    Returns:
+        List of dependencies (file name with absolute path)
+    """
     if not is_ruby_file(file_name):
         raise ValueError("Not Ruby file!")
     dependencies = []
